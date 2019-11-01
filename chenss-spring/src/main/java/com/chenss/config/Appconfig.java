9ca1b665f27f3848b9fbe0c3e91be0af23fb365a
chendushuai <chendushuai@126.com>
2019-11-02 00:47:58 +0800

    修改中文注解说明

--- chenss-spring/src/main/java/com/chenss/config/Appconfig.java
+++ chenss-spring/src/main/java/com/chenss/config/Appconfig.java
@@ -1,13 +1,25 @@
 package com.chenss.config;
 
+import com.chenss.dao.BeanTestA;
+import com.chenss.dao.BeanTestB;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
 
-//@Configuration
+@Configuration
 @Component
 @ComponentScan("com.chenss")
 //@ImportResource("classpath:spring.xml")
 public class Appconfig {
+	@Bean
+	public BeanTestA getBeanTestA() {
+		return new BeanTestA();
+	}
+	@Bean
+	public BeanTestB getBeanTestB() {
+		getBeanTestA();
+		return new BeanTestB();
+	}
 }
---
+++ chenss-spring/src/main/java/com/chenss/dao/BeanTestB.java
@@ -0,0 +1,7 @@
+package com.chenss.dao;
+
+public class BeanTestB {
+	public BeanTestB() {
+		System.out.println("BeanTestB init" +this.hashCode());
+	}
+}
