450c32ccb6dc1f2e3488e46649a09a0ba5645597
chendushuai <chendushuai@126.com>
2019-11-10 09:16:46 +0800

    AOP可运行版本

--- chenss-spring/src/main/java/com/chenss/dao/ChenssFactoryBean.java
+++ chenss-spring/src/main/java/com/chenss/dao/ChenssFactoryBean.java
@@ -2,19 +2,4 @@ package com.chenss.dao;
 
 import org.springframework.beans.factory.FactoryBean;
 
-public class ChenssFactoryBean implements FactoryBean {
-	@Override
-	public Object getObject() throws Exception {
-		return new UserDaoImpl();
-	}
-
-	@Override
-	public Class<?> getObjectType() {
-		return UserDaoImpl.class;
-	}
-
-	@Override
-	public boolean isSingleton() {
-		return true;
-	}
-}
+public class ChenssFactoryBean {}
